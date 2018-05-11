0dd44a5c39f2ac1aa5dd2db867cc232438e967d0
rmonla <rmonla@gmail.com>
2018-05-11 11:47:09 -0300

    rmDUYTube.sh: Versión Stable.

--- rmDUYTube.sh
+++ rmDUYTube.sh
@@ -16,7 +16,7 @@ exitcode=1
 ##
 ##########################################################################
 # moz_usage()
-rm_uso()
+duy_uso()
 {
 echo ""
 echo "Uso:  ${rmApp} -u URL -a ARCHIVO [-t TITULO]"
@@ -36,7 +36,7 @@ echo ""
 	return 0
 }
 ##########################################################################
-rm_msg()
+duy_msg()
 {
 	msg=$1
 	echo
@@ -46,7 +46,7 @@ rm_msg()
 	exit 1
 }
 ##########################################################################
-rm_run_app()
+duy_run()
 {
 	app=$rmApp
 	##
@@ -54,38 +54,28 @@ rm_run_app()
 	##
 	if [ ! -x "$app" ]
 	then
-		rm_msg "No puede ejecutarce el script --> $app"
+		duy_msg "No puede ejecutarce el script --> $app"
 	fi
 	##
 	## Ejecuto el script.
 	##
 	echo
 	echo "App:     $app"
-	echo "Url:     $duy_url"
-	echo "Archivo: $duy_arch"
-	echo "Título:  $duy_tit"
+	echo "Url:     $duyURL"
+	echo "Archivo: $duyARCH"
+	echo "Título:  $duyTIT"
 	echo
 	
-	# duy_cmd="python rmDUYTube.py -u $duy_url -o $duy_arch"
-
-	# exec "$duy_cmd" ${1+"$@"}
-	python rmDUYTube.py "-u $duy_url -o $duy_arch -t $duy_tit"
+	python rmDUYTube.py -u "$duyURL" -o "$duyARCH" -t "$duyTIT"
 	exit 1
-	
-	
-
-# cd /home/rmonla/rmFiles/rmDocs/DTIC/Scripts/rm_duytb
-# screen python rmDUYTube.py -u http://190.114.222.202/tcs/download/0D88714D-0490-4D75-BF4A-3126CD5E1A90 -o TSAyGIES_Gestión_C10_24Abr18
-# screen python rmDUYTube.py -u http://190.114.222.202/tcs/download/034027E7-224E-49CE-8597-8AEDF519AF46 -o TSAyGIES_Gestión_C11_26Abr18
-
 
 }
 ##########################################################################
 ##
 ## Predetermino argumentos
 ##
-duy_url=""
-duy_arch=""
+duyURL=""
+duyARCH=""
 #
 ##
 ## Parse the command line
@@ -94,40 +84,40 @@ while [ $# -gt 0 ]
 do
   case $1 in
     -u | --url)
-      duy_url=$2;
-      if [ "${duy_url}" != "" ]; then
+      duyURL=$2;
+      if [ "${duyURL}" != "" ]; then
 	shift 2
       else
-        rm_uso
-        rm_msg "Falta URL --> -u URL"
+        duy_uso
+        duy_msg "Falta URL --> -u URL"
         exit 1
       fi
       ;;
     -a | --archivo)
-      duy_arch=$2;
-      if [ "${duy_arch}" != "" ]; then
+      duyARCH=$2;
+      if [ "${duyARCH}" != "" ]; then
 	shift 2
       else
-        rm_uso
-        rm_msg "Falta nombre del ARCHIVO de salida --> -a ARCHIVO"
+        duy_uso
+        duy_msg "Falta nombre del ARCHIVO de salida --> -a ARCHIVO"
         exit 1
       fi
       ;;
     -t | --titulo)
-      duy_tit=$2;
-      if [ "${duy_tit}" != "" ]; then
+      duyTIT=$2;
+      if [ "${duyTIT}" != "" ]; then
 	shift 2
       else
-        duy_tit=$duy_arch
+        duyTIT=$duyARCH
       fi
       ;;
     -h | --help)
-      rm_uso
+      duy_uso
       exit 1
       ;;
     *)
-      rm_uso
-      rm_msg "No se reconoce el parámetro --> $1"
+      duy_uso
+      duy_msg "No se reconoce el parámetro --> $1"
       exit 1
       break;
       ;;
@@ -135,22 +125,19 @@ do
 done
       
 req=0
-if [ "${duy_url}" != "" ]; then
-	if [ "${duy_arch}" != "" ]; then
+if [ "${duyURL}" != "" ]; then
+	if [ "${duyARCH}" != "" ]; then
 		req=1
 	fi
 fi
 
 if [ "${req}" != 0 ]; then
-	# rm_run_app
-	rm_run_app ${1+"$@"}
+	# duy_run
+	duy_run ${1+"$@"}
 else
-	rm_uso
-	rm_msg "Faltan parámetros."
+	duy_uso
+	duy_msg "Faltan parámetros."
 	exit 1
 fi
 
-# if [ "${duy_url}" != "" or "${duy_arch}" != "" ]
-
-
-exit $exitcode
+exit 1
