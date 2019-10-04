fb16987d3ce6e438be06d907476d6632780cab3f
Ricardo MONLA <rmonla@gmail.com>
2019-10-04 20:25:35 -0300

    actu 19100402

---
+++ Descargas.sh
@@ -0,0 +1,78 @@
+Descargas
+
+screen ./rmDUY \
+-a 'LAyGIES_Finanzas04_16Sep19_14:00' \
+-t 'LAyGIES - Finanzas - C04 - 16Sep19_14:00' \
+-D 'Clase 04 - Luís CARRETERO - Finanzas y Financiamiento en IES - LAyGIES - UTNMendoza' \
+-u http://190.114.222.202/tcs/download/315DD7B1-5BE2-4A11-801C-DFBADA79D217
+
+'
+_*Última Clase:*_
+-->> http://bit.ly/LAyGIES_Fnzas_C04
+_*Lista de Clases:*_
+-->> http://bit.ly/LAyGIES_Fnzas_Clases
+_*Apuntes de Clases:*_
+-->> http://bit.ly/LAyGIES_Fnzas_Apuntes
+'
+
+screen ./rmDUY \
+-a 'LAyGIES_Epist_C06_2Oct19_14:00' \
+-t 'LAyGIES - Epistemología - C06_2Oct19_14:00' \
+-D 'Clase 06 - Victor OLIVA - Epistemología - LAyGIES - UTNMendoza' \
+-u http://190.114.222.202/tcs/download/8CB65F9C-557A-41F3-9498-AE6F617CDFA0.mp4
+
+'
+_*Última Clase:*_
+-->> https://youtu.be/MW82OTYnIT8
+_*Lista de Clases:*_
+-->> http://bit.ly/LAyGIES_Epist_Clases
+_*Apuntes de Clases:*_
+-->> http://bit.ly/LAyGIES_Epist_Apuntes
+'
+
+screen ./rmDUY \
+-a 'LAyGIES_TIC06_04Oct19_14:00' \
+-t 'LAyGIES - TIC - C06 - 04Oct19_14:00' \
+-D 'Clase 06 - Ing. Anibal CATAPANO GILI - Tecnología de la Información y Comunicaciones en IES - LAyGIES - UTNMendoza' \
+-u http://190.114.222.202/tcs/download/4EC4AC1D-DFF2-4B08-BB23-35638AA0A6D5.mp4
+
+'
+_*Última Clase:*_
+-->> http://bit.ly/LAyGIES_TIC_C04
+_*Lista de Clases:*_
+-->> http://bit.ly/LAyGIES_TIC_Clases
+_*Apuntes de Clases:*_
+-->> http://bit.ly/LAyGIES_TIC_Apuntes
+'
+
+
+
+### UTNLaRioja_SSAE
+screen ./rmDUY \
+-a 'SSAE_JDTecnologicos_3Oct19_14:00' \
+-t 'UTNLaRioja_SSAE - Juegos Deportivos Tecnológicos - 3Oct19_14:00' \
+-D 'Juegos Deportivos Tecnológicos' \
+-u http://190.114.222.202/tcs/download/CD8F01FE-BB19-4AAF-A4D5-AE233C60F02D.mp4
+
+'
+_*Última Video:*_
+-->> https://youtu.be/0L4iT6heX5s
+_*Lista de Videos:*_
+-->> http://bit.ly/UTNLaRioja_SSAE_VCs
+'
+
+### UTNLaRioja_TIC
+screen ./rmDUY \
+-a 'UTNLaRioja_TIC_ZOOM_26Sept_17:00' \
+-t 'UTNLaRioja_TIC - UTN Implementó ZOOM - 26Sept_17:00' \
+-D 'UTN Implementó ZOOM, la Plataforma de Comunicación en la Nube - https://utn.edu.ar/es/?option=com_content&view=article&id=537' \
+-u http://portalvc.utn.edu.ar:8081/storage-01/2019/videos/2019.09.26-17.00-PRESENTACION_ZOOM-UTN.mp4
+
+'
+_*Última V-Conferencia:*_
+-->> https://youtu.be/8hWzEkyBEZM
+_*Lista de Videos:*_
+-->> http://bit.ly/UTNLaRioja_SSAE_VCs
+'
+
+
